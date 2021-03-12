847ad438fd5e5101f408f7f1e98663d0b888d571
noplur <geoduckphoto@gmail.com>
2021-03-12 01:32:04 -0800

    update to new post link

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,18 +41,6 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
-
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//       res.redirect('/');
-//       return;
-//   }
-//   res.render('login');
-// });
-
-// router.get('/signup', (req, res) => {
-//     res.render('signup');
-// });
   
   // get single post
 
