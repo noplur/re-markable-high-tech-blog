98dca61031ccc7d7cf87cb36be1dd9657c074cb7
noplur <geoduckphoto@gmail.com>
2021-03-13 00:26:17 -0800

    updated comment styling

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -89,45 +89,45 @@ Post.findOne({
   });
 });
 
-router.get('/posts-comments', (req, res) => {
-Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'content',
-            'title',
-            'created_at'
-        ],
-        include: [{
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostData => {
-        if (!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id' });
-            return;
-        }
-        const post = dbPostData.get({ plain: true });
+// router.get('/posts-comments', (req, res) => {
+// Post.findOne({
+//         where: {
+//             id: req.params.id
+//         },
+//         attributes: [
+//             'id',
+//             'content',
+//             'title',
+//             'created_at'
+//         ],
+//         include: [{
+//                 model: Comment,
+//                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//                 include: {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             },
+//             {
+//                 model: User,
+//                 attributes: ['username']
+//             }
+//         ]
+//     })
+//     .then(dbPostData => {
+//         if (!dbPostData) {
+//             res.status(404).json({ message: 'No post found with this id' });
+//             return;
+//         }
+//         const post = dbPostData.get({ plain: true });
 
-        res.render('posts-comments', { post, loggedIn: req.session.loggedIn });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+//         res.render('posts-comments', { post, loggedIn: req.session.loggedIn });
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
   
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
