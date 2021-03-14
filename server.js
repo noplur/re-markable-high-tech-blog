f60bab09730133f4c60cffd9d54f0b3d8c429b39
noplur <geoduckphoto@gmail.com>
2021-03-13 21:00:25 -0800

    set time interval

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Really secret',
-  cookie: {},
+  cookie: { maxAge: 600000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
