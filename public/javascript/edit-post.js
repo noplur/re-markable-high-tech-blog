bcaf98b465f205f471ba6d1c2ae1ffb0394f0f8e
noplur <geoduckphoto@gmail.com>
2021-03-13 18:49:15 -0800

    edit posts success

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,14 +1,18 @@
 async function editFormHandler(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const title = document.querySelector('textarea[name="title"]').value.trim();
+    const content = document.querySelector('textarea[name="content"]').value.trim();
+
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/posts/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title
+        post_id: id,
+        title, 
+        content
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -22,5 +26,5 @@ async function editFormHandler(event) {
     }
   }
   
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  document.querySelector('#edit-post-form')?document.querySelector('#edit-post-form').addEventListener('submit', editFormHandler):null;
   
